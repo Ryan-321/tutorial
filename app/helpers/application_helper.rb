e412c7327cb2c7a17ecca43ad655978561b0ecfc
ryan.kahle <ryan.kahle@theeagleforce.net>
2016-07-13 09:00:54 -0400

    Adds application_helper for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '') # Passing in a default value for page_title, unless otherwise expressed by call
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
