5c383413c36e882bcffbc2a57262a9223a644386
Ryan Kahle <jrkahle@hotmail.com>
2016-08-26 07:33:03 -0400

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -95,4 +95,22 @@ class UserTest < ActiveSupport::TestCase
     ryan.unfollow(archer)
     assert_not ryan.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    ryan = users(:ryan)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert ryan.feed.include?(post_following)
+    end
+    # Posts from self
+    ryan.microposts.each do |post_self|
+      assert ryan.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not ryan.feed.include?(post_unfollowed)
+    end
+  end
 end
